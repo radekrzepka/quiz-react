a79c3004218462d22e66eb18e94bab959ae08652
Radosław Rzepka <radek.rz.123@gmail.com>
2022-05-26 01:32:55 +0200

    added styling to CategoryPicker

--- src/components/CategoryPicker/CategoryPicker.js
+++ src/components/CategoryPicker/CategoryPicker.js
@@ -1,13 +1,30 @@
 import questions from "../../data/questions.json";
-// import styles from "./CategoryPicker.module.css";
+import styles from "./CategoryPicker.module.css";
 
 const CategoryPicker = props => {
 	return (
-		<div>
+		<div className={styles["category-picker"]}>
+			<h1 className={styles.header}>Quizy</h1>
+			<p className={styles.paragraph}>Wybierz kategorię, aby rozpocząć quiz</p>
 			{Object.keys(questions).map((category, index) => (
-				<div key={index} onClick={() => props.selectCategory(category)}>
-					{questions[category].name}
-					{questions[category].icon}
+				<div
+					key={index}
+					onClick={() => props.selectCategory(category)}
+					className={styles.category}
+				>
+					<img
+						src={`icons/${questions[category].icon}`}
+						alt={`${questions[category].name} ikona`}
+						className={styles.image}
+					/>
+					<p className={styles.text}>
+						{questions[category].name} ({questions[category].questions.length}{" "}
+						{questions[category].questions.length >= 22 &&
+						questions[category].questions.length <= 24
+							? "pytania"
+							: "pytań"}
+						)
+					</p>
 				</div>
 			))}
 		</div>
