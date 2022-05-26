fbb79f87a4c33d9228e422e624c856507020b20e
Radosław Rzepka <radek.rz.123@gmail.com>
2022-05-26 23:05:33 +0200

    added styling to Ranking

--- src/components/Ranking/Ranking.js
+++ src/components/Ranking/Ranking.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import questions from "../../data/questions.json";
 import buttonStyles from "../buttons/Button.module.css";
+import styles from "./Ranking.module.css";
 
 const Ranking = props => {
 	const [selectedCategory, setSelectedCategory] = useState("basketball");
@@ -11,23 +12,41 @@ const Ranking = props => {
 		(a, b) => b.score - a.score || a.resolvingTime - b.resolvingTime
 	);
 	return (
-		<div>
-			<label htmlFor="ranking">Wybierz kategorię: </label>
-			<button className={buttonStyles.button} onClick={props.hideRanking}>
-				Powrót
-			</button>
-			<select id="ranking" onChange={switchCategory}>
+		<div className={styles.ranking}>
+			<label className={styles.label} htmlFor="ranking">
+				Wybierz kategorię:{" "}
+			</label>
+			<select className={styles.select} id="ranking" onChange={switchCategory}>
 				{Object.keys(questions).map((category, index) => (
 					<option value={category} key={index}>
 						{questions[category].name}
 					</option>
 				))}
 			</select>
-			{sortedResults.map((result, index) => (
-				<div key={index}>
-					{index + 1}. {result.name} {result.score}
-				</div>
-			))}
+			<div className={styles.results}>
+				{sortedResults.map((result, index) => (
+					<p
+						key={index}
+						className={`${index <= 2 ? styles["best-results"] : ""} ${
+							styles.result
+						}`}
+					>
+						{index + 1}. {result.name} {result.score}/
+						{questions[selectedCategory].questions.length}{" "}
+						{Math.floor(result.resolvingTime / (60 * 10))}:
+						{Math.floor(
+							result.resolvingTime / 10 -
+								Math.floor(result.resolvingTime / (60 * 10)) * 60
+						)}
+					</p>
+				))}
+				<button
+					className={`${buttonStyles.button} ${styles.button}`}
+					onClick={props.hideRanking}
+				>
+					Powrót
+				</button>
+			</div>
 		</div>
 	);
 };
